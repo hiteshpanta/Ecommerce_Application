e782ebba1fa621c4d754c98aa452eb16c08c661b
__Hitesh__ <--hiteshpant50@gmail.com>
2026-04-13 22:23:51 +0545

    deployment setup

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,33 @@
-import { defineConfig } from 'vite'
 import path from "path"
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: 
-  [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+
+   
+    chunkSizeWarningLimit: 1200,
+
+    rollupOptions: {
+      output: {
+        // Auto split big node_modules into separate chunks
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+          }
+        },
+      },
     },
   },
 })
