470f8b8740fdddd99de77062c94938dee20835c7
__Hitesh__ <--hiteshpant50@gmail.com>
2026-04-13 22:27:51 +0545

    cors origin setup

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.DB_URL).then((val)=>{
 });
 
 app.use(cors({
-    origin: []
+    origin: [ 'https://ecommerce-application-peach.vercel.app/', 'http://localhost:5000']
 }   
 ));
 app.use(express.json());
